4e85aaeee2f34b19f45096a428062f7503f33cce
HayBael <91926013+HayBael@users.noreply.github.com>
2024-10-28 15:54:42 +0700

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,7 @@ import { Box } from '@chakra-ui/react'
 const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
-      {new Date().getFullYear()} Made With 💖 And JS.
+      {new Date().getFullYear()} Made In Ujungberung, China
     </Box>
   )
 }
