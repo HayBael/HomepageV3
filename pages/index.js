5034f67b15bf48082362f2cabbe5c8f44b10871e
HayBael <91926013+HayBael@users.noreply.github.com>
2024-11-05 09:51:53 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -154,7 +154,7 @@ const Home = () => (
                 colorScheme="purple"
                 leftIcon={<IoLogoInstagram />}
               >
-                 @wargadarat
+               @wargadarat
               </Button>
             </Link>
           </ListItem>
