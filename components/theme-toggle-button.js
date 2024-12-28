3432804d9aa50b5f549af3ef8654e181cd82a76c
CodeMaple <91926013+HayBael@users.noreply.github.com>
2024-12-28 20:25:10 +0700

    Update theme-toggle-button.js

--- components/theme-toggle-button.js
+++ components/theme-toggle-button.js
@@ -1,25 +1,13 @@
-import React, { useEffect, useState } from 'react';
 import { IoSunny, IoMoon } from 'react-icons/io5';
 
-export default function ThemeToggle() {
-  const [theme, setTheme] = useState('light');
-
-  const toggleTheme = () => {
-    const newTheme = theme === 'light' ? 'dark' : 'light';
-    document.documentElement.classList.toggle('dark', newTheme === 'dark');
-    localStorage.setItem('theme', newTheme);
-    setTheme(newTheme);
-  };
-
-  useEffect(() => {
-    const savedTheme = localStorage.getItem('theme') || 'light';
-    document.documentElement.classList.toggle('dark', savedTheme === 'dark');
-    setTheme(savedTheme);
-  }, []);
-
+export default function ThemeToggle({ theme, toggleTheme }) {
   return (
-    <button onClick={toggleTheme} className="p-2 bg-gray-300 dark:bg-gray-800 rounded">
-      {theme === 'light' ? <IoSunny /> : <IoMoon />}
+    <button
+      onClick={toggleTheme}
+      className="p-2 rounded-full bg-gray-200 dark:bg-gray-800"
+      aria-label="Toggle Theme"
+    >
+      {theme === 'light' ? <IoSunny size={24} /> : <IoMoon size={24} />}
     </button>
   );
 }
