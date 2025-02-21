9cd83f76c1f069af793270c67ce9b07aa863f9c6
CodeMaple <91926013+HayBael@users.noreply.github.com>
2025-02-21 20:52:42 +0700

    Update theme-toggle-button.js

--- components/theme-toggle-button.js
+++ components/theme-toggle-button.js
@@ -1,35 +1,32 @@
-import { Icon, HStack, useColorMode } from "@chakra-ui/react";
-import { Switch } from "@chakra-ui/react";
-import { motion, AnimatePresence } from "framer-motion";
-import { IoIosRadioButtonOff, IoIosRadioButtonOn } from "react-icons/io";
+import { AnimatePresence, motion } from "framer-motion";
+import { IconButton, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import { IoIosRadioButtonOn, IoIosRadioButtonOff } from "react-icons/io";
 
-const ThemeToggleSwitch = () => {
-  const { colorMode, toggleColorMode } = useColorMode();
+const ThemeToggleButton = () => {
+  const { toggleColorMode } = useColorMode();
 
   return (
-    <HStack>
-      <AnimatePresence mode="wait" initial={false}>
-        <motion.div
-          key={colorMode}
-          initial={{ y: -10, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          exit={{ y: 10, opacity: 0 }}
-          transition={{ duration: 0.2 }}
-        >
-          <Icon color={colorMode === "light" ? "yellow.400" : "gray.400"}>
-            {colorMode === "light" ? <IoIosRadioButtonOn /> : <IoIosRadioButtonOff />}
-          </Icon>
-        </motion.div>
-      </AnimatePresence>
-
-      <Switch
-        colorScheme="blue"
-        size="lg"
-        isChecked={colorMode === "dark"}
-        onChange={toggleColorMode}
-      />
-    </HStack>
+    <AnimatePresence mode="wait" initial={false}>
+      <motion.div
+        style={{ display: "inline-block" }}
+        key={useColorModeValue("light", "dark")}
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: 20, opacity: 0 }}
+        transition={{ duration: 0.2 }}
+      >
+        <IconButton
+          aria-label="Toggle theme"
+          colorScheme={useColorModeValue("purple", "orange")}
+          icon={useColorModeValue(
+            <IoIosRadioButtonOn />,
+            <IoIosRadioButtonOff />
+          )}
+          onClick={toggleColorMode}
+        />
+      </motion.div>
+    </AnimatePresence>
   );
 };
 
-export default ThemeToggleSwitch;
+export default ThemeToggleButton;
