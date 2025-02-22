4fc5f7beb2534ed3288e31f6979e7cd1d86c8b69
CodeMaple <91926013+HayBael@users.noreply.github.com>
2025-02-22 11:39:08 +0700

    Update footprint.js

--- components/icons/footprint.js
+++ components/icons/footprint.js
@@ -1,17 +1,13 @@
-import Image from 'next/image'
-const FootprintIcon = props => {
-  return (
-    // eslint-disable-next-line jsx-a11y/alt-text
-    <Image
-      width="20"
-      height="20"
-      alt='Frazix'
-      className="w-5 inline-block transition-transform group-hover:rotate-[20deg]"
-      viewBox="0 0 40 40"
-      {...props}
-      src="/images/frazix.png"
-    />
-  )
-}
+import { DiLinux } from "react-icons/di";
 
-export default FootprintIcon
+const FootprintIcon = (props) => {
+  return (
+    <DiLinux
+      size={20}
+      className="w-5 inline-block transition-transform group-hover:rotate-[20deg]"
+      {...props}
+    />
+  );
+};
+
+export default FootprintIcon;
